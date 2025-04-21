bca5664d6fb9b757b3d352fa239ce5fdf5923f40
PUJARISRIDURGADEVI <devipujari237@gmail.com>
2025-04-21 18:06:03 +0530

    day_2

---
+++ day_2.py
@@ -0,0 +1,24 @@
+# a=0.25
+# b="hello"
+# c=-1.25
+# d=False
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+# a=input("enter a number")
+# b=input("enter a number")
+# c= int(a)+int(b)
+# print("a and b",c)
+# print(f"the sum of {a} and {b} is {c}")
+# a=input("enter your birth year")
+# current_year=2025
+# age=current_year-int(a)
+# print(age)
+forest=input("enter the number of coins to collect in forest: ") 
+lake=input("enter the number of coins to collect in lake: ")
+castle=input("enter the number of coins to collect in castle: ")
+total_coins=int(forest)+int(lake)+int(castle)
+print("total coins collected are",total_coins)
+
