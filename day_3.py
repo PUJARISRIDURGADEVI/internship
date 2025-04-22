917ad282c3a5ca26745e658d3483ace28a70924f
PUJARISRIDURGADEVI <devipujari237@gmail.com>
2025-04-22 18:28:01 +0530

    day 3

---
+++ day_3.py
@@ -0,0 +1,7 @@
+a=int(input("enter the marks: "))
+if a>60:
+    print("excellent")
+elif a>80:
+    print("good")
+else:
+    print("need to improve")
---
+++ work1.py
@@ -0,0 +1,13 @@
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# print(f" add of {a} and {b} is {a+b}, sub of {a} and {b} is {a-b}, mul of {a} and {b} is {a*b}, div of {a} and {b} is {a/b}")
+# integer
+# age=20
+# print(type(age))
+# gpa=0.25
+# print(type(gpa))
+# name="hello"
+# print(type(name))
+is_student=True
+print(type(is_student))
+print(is_student)
